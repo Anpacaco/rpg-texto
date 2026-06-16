b3e78afd8230caefaef6a4010e6c277f42df908c
ANTONIO PAULO CABRAL CONTENTE <antonio_contente@estudante.sesisenai.org.br>
2026-06-16 16:10:58 -0300

    função para exibir informações do herói

---
+++ heroi.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+void exibir(string nome, int vida, int ataque){
+	cout<<"---"<<nome<<"---";
+	cout<<"\nVida: "<<vida;
+	cout<<"\nAtaque: "<<ataque;
+	
+}
