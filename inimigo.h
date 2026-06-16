94b5dc8cac26debbca1b650262ea9b3cb7c20699
ANTONIO PAULO CABRAL CONTENTE <antonio_contente@estudante.sesisenai.org.br>
2026-06-16 16:12:46 -0300

    função para exibir informações do inimigo

---
+++ inimigo.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+void encontro(string nome, int vida, int defesa, int ataque){
+	cout<<"Inimigo: "<<nome;
+	cout<<"\nVida: "<<vida;
+	cout<<"\nDefesa: "<<defesa;
+	cout<<"\nAtaque: "<<ataque;
+}
