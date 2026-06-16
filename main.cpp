57beb11a8fd8cd7c932c6eab8b08bb37ad2996e5
ANTONIO PAULO CABRAL CONTENTE <antonio_contente@estudante.sesisenai.org.br>
2026-06-16 16:13:52 -0300

    Programa que exibe os status dos personagens

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "heroi.h"
+#include "inimigo.h"
+using namespace std;
+
+int main(){
+	exibir("Jeremias", 140, 60);
+	cout<<"\n\n";
+	encontro("Esqueleto", 20, 10, 70);
+}
