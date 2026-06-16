1015eabfe427947dcce23dc00edac66104a0c72c
ANTONIO PAULO CABRAL CONTENTE <antonio_contente@estudante.sesisenai.org.br>
2026-06-16 16:40:31 -0300

    Função para exibir informações do herói

--- heroi.h
+++ heroi.h
@@ -3,9 +3,10 @@
 #include<string>
 using namespace std;
 
-void exibir(string nome, int vida, int ataque){
+void exibir(string nome, int vida, int defesa, int ataque){
 	cout<<"---"<<nome<<"---";
 	cout<<"\nVida: "<<vida;
+	cout<<"\nDefesa: "<<defesa;
 	cout<<"\nAtaque: "<<ataque;
 	
 }
